cca8814b9a63b1c347c0267d6ce4ed6e31cfcdb7
Lohyc <lohyuchen12345@gmail.com>
2021-04-01 21:48:31 +0800

    reorg

--- api/main.py
+++ api/main.py
@@ -1,9 +1,9 @@
 import os
 import pymongo
+from pydantic import BaseModel
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
 
 
 client = pymongo.MongoClient(os.getenv("MONGODB_URL"))
