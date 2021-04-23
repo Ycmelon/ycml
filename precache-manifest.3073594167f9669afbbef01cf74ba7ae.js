043573ee40cb7928a4020db27d83bf3644abf08a
Ycmelon <lohyuchen12345@gmail.com>
2021-04-23 14:56:25 +0000

    Deploying to gh-pages from @ Ycmelon/ycml@525ada053a80adecfd9da7ca35656a0e09c810b5 🚀

---
+++ precache-manifest.3073594167f9669afbbef01cf74ba7ae.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "450ccacc068bc3cb0ce226dd8e51e587",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8cda4f79fb7ddcae1fcb",
+    "url": "./static/css/2.104d6ef2.chunk.css"
+  },
+  {
+    "revision": "fdb6bb690deeb087a269",
+    "url": "./static/css/main.ca151bf9.chunk.css"
+  },
+  {
+    "revision": "8cda4f79fb7ddcae1fcb",
+    "url": "./static/js/2.085ef8c2.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "./static/js/2.085ef8c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdb6bb690deeb087a269",
+    "url": "./static/js/main.4628f536.chunk.js"
+  },
+  {
+    "revision": "52ee92a4e48b4577e4e9",
+    "url": "./static/js/runtime-main.247ede64.js"
+  },
+  {
+    "revision": "66eaad04066b97bba992c16120836631",
+    "url": "./static/media/content-cut.66eaad04.svg"
+  },
+  {
+    "revision": "d150d4d60b034a7b8fc0df36dd389590",
+    "url": "./static/media/github.d150d4d6.svg"
+  },
+  {
+    "revision": "73f26bf98a715ecab4d2287ff3a02ad0",
+    "url": "./static/media/roboto-all-400-normal.73f26bf9.woff"
+  },
+  {
+    "revision": "ba2c6cb0af81f8da49a960db84f26b7c",
+    "url": "./static/media/roboto-cyrillic-400-normal.ba2c6cb0.woff2"
+  },
+  {
+    "revision": "2e0b8660b87034ecf56aa9f488fbc08e",
+    "url": "./static/media/roboto-cyrillic-ext-400-normal.2e0b8660.woff2"
+  },
+  {
+    "revision": "22786f243202d7912399ffd10c76fe78",
+    "url": "./static/media/roboto-greek-400-normal.22786f24.woff2"
+  },
+  {
+    "revision": "71683b6b55887ef2986bff04e1a841d6",
+    "url": "./static/media/roboto-greek-ext-400-normal.71683b6b.woff2"
+  },
+  {
+    "revision": "aa23b7b4bcf2b8f0e876106bb3de69c6",
+    "url": "./static/media/roboto-latin-400-normal.aa23b7b4.woff2"
+  },
+  {
+    "revision": "718dded3393324e992b225ac61329e0c",
+    "url": "./static/media/roboto-latin-ext-400-normal.718dded3.woff2"
+  },
+  {
+    "revision": "756af8e5560d200e53b52b1ff70f2ad0",
+    "url": "./static/media/roboto-vietnamese-400-normal.756af8e5.woff2"
+  },
+  {
+    "revision": "d03bd9f8907a3e65870b4aa0ca7f0d2b",
+    "url": "./static/media/scissors-cutting.d03bd9f8.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.83b8ab66b6ef5e60e02dc196d8ce6f5d.js"
+  "./precache-manifest.3073594167f9669afbbef01cf74ba7ae.js"
 );
 
 self.addEventListener('message', (event) => {
